236e2c7817600f5913081d49e56c5d956f35a405
vegaj <jvegamoreno@outlook.com>
2018-10-16 15:05:44 +0200

    added configuration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
+require('./config/config')
+
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+//parse application/json
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.json({ msg: "Hello, World!!" })
+})
+
+app.post('/', (req, res) => {
+
+    let body = req.body;
+
+    if (body.name === undefined) {
+        res.status(400).json({ ok: false, err: 'name required' })
+    } else {
+        res.json({
+            body
+        });
+    }
 })
 
 
